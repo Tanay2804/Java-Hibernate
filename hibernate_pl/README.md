ba8a65ecd6d1f1302fdf9f9762d2a860bdde9027
Tanay Tibrewal <archanatanay17@gmail.com>
2025-03-30 15:48:15 +0530

    Hibernate Setup with MySQL Complete, Refer properties and Main.java files

---
+++ hibernate_pl/README.md
@@ -0,0 +1,7 @@
+# To run a particular source file, use the command:
+
+```bash
+mvn clean -q
+mvn compile -q
+mvn exec:java -Dexec.mainClass="com.tanay.<Class or FileName>"
+```
--- hibernate_pl/src/main/java/com/tanay/Main.java
+++ hibernate_pl/src/main/java/com/tanay/Main.java
@@ -1,7 +1,48 @@
 package com.tanay;
+import java.io.FileInputStream;
+import java.util.Properties;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Product product = new Product();
+        // Note ID is auto-incremented, so we don't need to set it
+        product.setName("Laptop");
+        product.setPrice(999.99);
+        
+        // Once we have the details and the object is ready, we can use hibernate to save it to the database
+        //  we need to tell hibernate about the database connection and credentials
+        //  we need to create a configuration object and set the properties
+        // Go to resources and create a config file and specify the database connection details
+        // Refer: https://docs.jboss.org/hibernate/orm/6.6/quickstart/html_single/#obtaining
+        Configuration configuration = new Configuration();
+
+        // Load the properties file
+        Properties properties = new Properties();
+		properties.load(new FileInputStream("src/main/resources/hibernate.properties"));
+
+        // NOTE: Tell hibernate about the Product class to use as a schema and generate queries
+        configuration.addAnnotatedClass(com.tanay.Product.class);
+        configuration.addProperties(properties);
+        
+        // Create a session factory and open a session
+        SessionFactory factory = configuration.buildSessionFactory();
+        Session session = factory.openSession();
+
+        // Start a transaction
+        session.beginTransaction(); // Returns a transaction object from org.hibernate.config.Transaction
+
+        // Save the product object to the database
+        session.persist(product);
+        
+        
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session & factory
+        session.close();
+        factory.close();
     }
 }
